f8ae3196bbdb1f07fad8a8abc48ae806e049d8a9
William_TM <root@MSI.localdomain>
2022-03-16 14:18:58 +0800

    Nomor_1

--- dart_test1/bin/dart_test1.dart
+++ dart_test1/bin/dart_test1.dart
@@ -1,5 +1,16 @@
 import 'package:dart_test1/dart_test1.dart' as dart_test1;
+import 'dart:io';
+
+void Factor(int n) {
+  for (int i = n; i >= 0; i--) {
+    if (n % i == 0) {
+      stdout.write("${i} ");
+    }
+  }
+}
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_test1.calculate()}!');
+  int n = 6;
+  print("Factor dari ${n}");
+  Factor(n);
 }
